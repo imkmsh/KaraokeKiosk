df18b39577218c5e3d15f231d106f4d687250c0c
imkmsh <imkmsh@gmail.com>
2023-06-16 23:41:52 +0900

    Update Register feature

---
+++ main.py
@@ -0,0 +1,49 @@
+admin_pw = "0000"
+members = []
+rooms = []
+income = []
+
+
+def register():
+    global members
+    id_check = True
+    id = input("등록할 ID를 입력하세요.")
+    for member in members:
+        if member["id"] == id:
+            id_check = False
+    if id_check:
+        pw = input("사용 가능한 ID입니다. 비밀번호를 설정하세요.\n")
+        pw_check = input("비밀번호를 한 번 더 입력하세요.\n")
+        if pw != pw_check:
+            print("비밀번호가 일치하지 않습니다. 다시 입력하세요.")
+        else:
+            members.append({"id": id, "pw": pw})
+            print("회원가입이 완료되었습니다.")
+    else:
+        register()
+    
+
+def admin():
+    global admin_pw
+    pw = input("관리자 페이지입니다. 비밀번호를 입력하세요.\n")
+    if pw != admin_pw:
+        print("잘못된 비밀번호입니다. 다시 입력하세요.")
+    else:
+        admin_action = input("비밀번호 변경 (1) / 매출 확인 (2)\n")
+        if admin_action == "1":
+            checking_new_pw = True
+            while checking_new_pw:
+                new_pw = input("변경할 비밀번호를 입력하세요.\n")
+                new_pw_check = input("변경할 비밀번호를 입력하세요.\n")
+                if new_pw == new_pw_check:
+                    checking_new_pw = False
+                    print("비밀번호가 변경되었습니다.")
+                    admin()
+        else:
+            print(f"매출 누적액입니다. \t{sum(income)} 원")
+            print(f"1회 최고 매출액입니다.\t {max(income)}")
+
+print("MS 코인 노래방에 오신 것을 환영합니다.")
+
+print("관리자 모드 (99)")
+is_member = int(input("회원 (1) / 비회원 (2) / 회원가입 (3)\n"))
